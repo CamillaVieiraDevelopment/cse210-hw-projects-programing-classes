0b74f5501e2fdf910999c62a95f394d520e52a5d
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2025-09-04 10:16:49 -0300

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+    Console.WriteLine("What is the magic number? ");
+int magic_number = int.Parse(Console.ReadLine());
+
+
+int guess = -1;
+
+
+while (guess != magic_number)
+
+{
+Console.WriteLine("What is your guess? ");
+guess = int.Parse(Console.ReadLine());
+
+if (guess == magic_number)
+{
+    Console.WriteLine("You guessed it!");
+}
+else if (guess  > magic_number)
+{
+    Console.WriteLine("The magic number is lower");
+}
+else
+    Console.WriteLine("The magic number is higher");
+}
+}
 }
