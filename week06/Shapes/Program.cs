718f71cff003df32132dd0b7271c0eccd51cc713
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2025-10-09 10:22:11 -0300

    program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+     // Testing Square individually
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square Color: {square.GetColor()}");
+        Console.WriteLine($"Square Area: {square.GetArea()}");
+
+        // Testing Rectangle individually
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"\nRectangle Color: {rectangle.GetColor()}");
+        Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+
+        // Testing Circle individually
+        Circle circle = new Circle("Green", 3);
+        Console.WriteLine($"\nCircle Color: {circle.GetColor()}");
+        Console.WriteLine($"Circle Area: {circle.GetArea()}");
+
+        // Testing polymorphism with a list
+        Console.WriteLine("\n=== Shapes List ===");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
